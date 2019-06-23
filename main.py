67f598388951e643a8b11fb0c417d5a1e5b26bcd
xxvms <tom@pcservicegroup.co.uk>
2019-06-23 19:57:21 +0100

    added comments form the tutorial

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from classes import *
+# another way is to do this
+# import classes
+# then fran = classes.HighSchoolStudent("frank")
+# or we can use from classes import [then name of class or method or * for all
 
 frank = HighSchoolStudent("frank")
 print("Printing from Classes file")
